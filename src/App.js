0594f6818bd27405a7aed9b7ac5fbdf8ec9745fa
张加强 <alan.zhang@elitesland.com>
2024-07-31 11:21:59 +0800

    fix: 修改样式

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import { Input, Table, Tree } from "antd";
-import { Chart } from "@antv/g2";
-import featureData from './world.zh.json'
-
+import { Chart, render } from "@antv/g2";
+import featureData from "./world.zh.json";
+import { Scatter } from "@antv/g2plot";
+import lemonIcon from './icon-lemon.svg'
 import {
   FileOutlined,
   FolderOpenTwoTone,
@@ -12,21 +13,35 @@ import {
 const { Search } = Input;
 
 function App() {
-  const [dataSource, setDataSource] = useState([]);
+  let scatterPlot = null
+  const [dataSource, setDataSource] = useState([
+    {
+      name: "上海公司",
+      date: "2024-04-01",
+      time: "2024-03-01",
+      currency: "美元",
+      price: "10000",
+    },
+  ]);
   const [treeData, setTreeData] = useState([
     {
-      title: "parent 1",
-      key: "0-0",
+      title: "父级",
+      key: "0",
       icon: <FolderOpenTwoTone />,
       children: [
         {
-          title: "leaf",
-          key: "0-0-0",
+          title: "子集1",
+          key: "1",
           icon: <FileOutlined />,
         },
         {
-          title: "leaf",
-          key: "0-0-1",
+          title: "子集2",
+          key: "2",
+          icon: <FileOutlined />,
+        },
+        {
+          title: "子集3",
+          key: "3",
           icon: <FileOutlined />,
         },
       ],
@@ -34,7 +49,7 @@ function App() {
   ]);
 
   const loadJsonData = async () => {
-    console.log(111)
+    console.log(111);
     const response = await fetch("/world.zh.json");
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -56,34 +71,36 @@ function App() {
     },
     {
       title: "国内播出",
-      dataIndex: "age",
-      key: "age",
+      dataIndex: "date",
+      key: "date",
     },
     {
       title: "签约时间",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "time",
+      key: "time",
     },
     {
       title: "签约币种",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "currency",
+      key: "currency",
     },
     {
       title: "单集价格",
-      dataIndex: "address",
-      key: "address",
+      dataIndex: "price",
+      key: "price",
+      render: (v) => (v ? Number(v).toLocaleString() : 0),
     },
   ];
 
   const renderMap = async () => {
-
-    var markedArea = ['泰国', '菲律宾', '老挝', '缅甸', '柬埔寨',];
-    featureData.features = featureData.features.filter(feature => markedArea.includes(feature.properties.name) );
+    var markedArea = ["泰国", "菲律宾", "老挝", "缅甸", "柬埔寨"];
+    featureData.features = featureData.features.filter((feature) =>
+      markedArea.includes(feature.properties.name)
+    );
 
     const map = new window.BMapGL.Map("allmap");
     var point = new window.BMapGL.Point(116.414, 39.915);
-    map.centerAndZoom(point, 10);
+    map.centerAndZoom(point, 1);
     map.enableScrollWheelZoom(true);
     map.setMapStyleV2({ styleJson: window.whiteStyle });
     var fillLayer = null;
@@ -135,77 +152,91 @@ function App() {
     fillLayer.setData(featureData);
   };
   const renderChart = () => {
-    const chart = new Chart({
-      container: "chartContainer",
-      autoFit: true,
-    });
-
     const chartData = [
       {
         name: "北京公司",
         month: 5,
         year: 2020,
-        color: "red",
       },
       {
         name: "杭州公司",
         month: 7,
         year: 2021,
-        color: "red",
       },
       {
         name: "青岛公司",
         month: 12,
         year: 2022,
-        color: "red",
       },
       {
         name: "上海公司",
         month: 10,
         year: 2023,
-        color: "red",
       },
       {
         name: "郑州公司",
         month: 3,
         year: 2024,
-        color: "bule",
       },
     ];
+if(!scatterPlot) {
+  scatterPlot = new Scatter("chartContainer", {
+    // padding: 40,
+    data: chartData,
+    xField: "year",
+    yField: "month",
+    size: 8,
+    shape: 'circle',
+    pointStyle: {
+      fill: 'orange',
+    },
+    tooltip: false,
 
-    chart
-      .point()
-      .data(chartData)
-      .encode("x", "year")
-      .encode("y", "month")
-      .encode("title", "name")
-      .axis("y", { title: "" })
-      .axis("x", { title: "" })
-      .axis("y", false)
-      .interaction("tooltip", {
-        render: (event, { title, items }) => `<div>
-        <h3 style="padding:0;margin:0">${title}</h3>
-        </div>`,
-      })
-      .label({
-        text: "name",
-        stroke: "#fff",
-        textAlign: "start",
-        textBaseline: "middle",
-        dx: 10,
-        position: "left",
-        fontSize: 10,
-        lineWidth: 2,
-      });
-
-    chart.render();
+    // tooltip: {
+    //   customContent: (title, data) => {
+    //     return data?.[0]?.data?.name;
+    //   },
+    // },
+      yAxis: {
+        label: false,
+      },
+    xAxis: {
+      min: 2019,
+      max: 2025,
+      grid: {
+        line: {
+          style: {
+            stroke: "#eee",
+          },
+        },
+      },
+      line: {
+        style: {
+          stroke: "#aaa",
+        },
+      },
+    },
+    label: {
+      formatter: (data)=>{
+        return data?.name
+      },
+    //    style: {
+    //   fill: 'red',
+    //   opacity: 0.6,
+    //   fontSize: 24
+    // }
+      },
+  });
+  scatterPlot.render();
+}
+    
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="page">
-          <div className="area title">地区数据</div>
+          <div className="area title"><img src={lemonIcon} alt="" />地区数据</div>
           <div className="left">
             <Search
               placeholder="请输入关键词"
@@ -224,15 +255,15 @@ function App() {
           <div id="allmap" className="map"></div>
           <div className="right">
             <div className="top">
-              <p className="title">公司合作情况</p>
+              <p className="title"><img src={lemonIcon} alt="" />公司合作情况</p>
               <div
                 id="chartContainer"
-                style={{ width: "440px", height: "400px" }}
+                style={{ width: "410px", height: "400px" }}
               />
             </div>
             <div className="bottom">
-              <p className="title">列表数据</p>
-              <Table columns={columns} dataSource={dataSource} />
+              <p className="title"><img src={lemonIcon} alt="" />列表数据</p>
+              <Table columns={columns} dataSource={dataSource} size="small" />
             </div>
           </div>
         </div>
