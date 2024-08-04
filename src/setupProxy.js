d010f631a4136f0b2830e104628ea13f58ee3060
张加强 <alan.zhang@elitesland.com>
2024-08-04 17:49:13 +0800

    fix(web): 添加代理

---
+++ src/setupProxy.js
@@ -0,0 +1,20 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+
+  // 你可以添加更多代理配置
+  app.use(
+    '/other-api',
+    createProxyMiddleware({
+      target: 'http://localhost:4000',
+      changeOrigin: true,
+    })
+  );
+};
